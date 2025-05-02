a0a622c87faedac588c478b99bf0decb0ff4b676
Zilong-Li <zimusen94@gmail.com>
2025-05-02 14:27:54 +0200

    refactor configuration

--- src/install.libs.R
+++ src/install.libs.R
@@ -3,3 +3,4 @@ files <- c(files, dir("htslib-1.21/", pattern="a$", full.names=TRUE))
 dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
+
