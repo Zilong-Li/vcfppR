4179420f1c518de05bc7287f6f3417d514a5fa8c
Zilong-Li <zimusen94@gmail.com>
2025-06-18 14:51:56 +0200

    cran version

--- src/install.libs.R
+++
@@ -1,6 +0,0 @@
-files <- Sys.glob(paste0("*", SHLIB_EXT))
-files <- c(files, dir("htslib-1.21/", pattern="a$", full.names=TRUE))
-dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
-dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-file.copy(files, dest, overwrite = TRUE)
-
